b67c62d6534162f9a05990a7d19ab70156731fcb
GiovanniConti <giovanni.conti@live.it>
2021-10-07 17:00:22 +0200

    Complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const usaerName = prompt("Inserisci il tuo nome.");
+const userSurname = prompt("Inserisci il tuo cognome");
+const userColore = prompt("Inserisci il tuo colore preferito");
+
+console.log(usaerName);
+console.log(userSurname);
+console.log(userColore);
+
+alert(`la tua password è: ${usaerName}${userSurname}${userColore}21`);
